0057951e1314ff2adf1f793b2a39b4495da1f65c
Felipe Gusmão <gusmao.dev@gmail.com>
2022-06-24 16:48:40 -0300

    Adição do Apollo e consumo da API em GraphQL

--- eventPlataform/src/App.tsx
+++ eventPlataform/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
+import { gql } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
 
+const GET_LESSONS_QUERY = gql`
+query {
+  lessons { 
+    id
+    title
+  }
+}
+`
+
+function App() {
+  useEffect(()=> {
+    client.query({
+      query: GET_LESSONS_QUERY,
+    }).then(response => {
+      console.log(response.data)
+    })
+  })
   return (
     <h1 className="text-2xl">Hello World</h1>
   )
