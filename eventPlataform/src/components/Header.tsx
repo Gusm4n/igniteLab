a0ba948b536b701acdf899a8c1ee3c9bb3e7785d
Felipe Gusmão <gusmao.dev@gmail.com>
2022-06-27 16:35:48 -0300

    adição do box de player de video, descrição e botões

--- eventPlataform/src/components/Header.tsx
+++ eventPlataform/src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Logo } from "./Logo";
 
 export function Header() {
     return (
-        <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-green-700-600">
+        <header className="w-full py-5 flex items-center justify-center bg-gray-900 border-green-700-600 border-b">
             <Logo />
         </header>
     )
